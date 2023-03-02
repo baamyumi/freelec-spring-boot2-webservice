3d9a2d06a51a7a17e14da1af35089a25a3d024ce
baamyumi <dbal5886@gmail.com>
2023-03-02 14:31:11 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/book/FreelecSpringBoot2WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringBoot2WebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelecSpringBoot2WebserviceApplication.class, args);
+    }
+
+}
