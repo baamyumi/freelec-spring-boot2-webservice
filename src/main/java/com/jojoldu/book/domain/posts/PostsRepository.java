9728956cac7a7b91f8fb0965c7c951e5a5965780
baamyumi <dbal5886@gmail.com>
2023-03-17 15:02:33 +0900

    2023-03-17

--- src/main/java/com/jojoldu/book/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/domain/posts/PostsRepository.java
@@ -1,6 +1,12 @@
 package com.jojoldu.book.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("Select p from Posts p order by p.id desc")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/jojoldu/book/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jojoldu.book.service.posts;
 
 import com.jojoldu.book.domain.posts.Posts;
 import com.jojoldu.book.domain.posts.PostsRepository;
+import com.jojoldu.book.web.dto.PostsListResponseDto;
 import com.jojoldu.book.web.dto.PostsResponseDto;
 import com.jojoldu.book.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,9 +34,24 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+        postsRepository.delete(posts);
     }
 }
