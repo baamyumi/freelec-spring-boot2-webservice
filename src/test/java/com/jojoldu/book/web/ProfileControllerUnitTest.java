eb92d7dd6eba81fc6c1eb3f69e25335fc477e204
baamyumi <dbal5886@gmail.com>
2023-03-30 10:22:26 +0900

    2023-03-30 무중단 배포 구성

---
+++ src/test/java/com/jojoldu/book/web/ProfileControllerUnitTest.java
@@ -0,0 +1,60 @@
+package com.jojoldu.book.web;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProfileControllerUnitTest {
+
+    @Test
+    @DisplayName("real profile 조회")
+    public void real_profile(){
+        String expectedProfile = "real";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    @DisplayName("real profile이 없으면 첫번째가 조회된다")
+    public void realProfileTest() {
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    @DisplayName("active profile이 없으면 default가 조회된다")
+    public void activeProfileTest() {
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
